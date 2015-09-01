9a15881e500a8a2dccc92e47224c6468a38687b2
akoch-ov <ako40ff@gmail.com>
2015-09-01 19:28:18 +0400

    config-params

---
+++ config/params-template.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'emailFrom' => 'w@yandex.ru',
+    'tokenExpire' => 3600,
+    'cookieExpire' => 24 * 3600,
+    'cacheExpire' => 3600,
+    'ym' => [
+        'client_id' => '',
+        'client_secret' => '',
+        'access_token' => ''
+    ]
+];
